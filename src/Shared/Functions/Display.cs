65bcbfb729fa0734665885c71ed2ceff86963df6
mosuchowski <mosuchowski04@gmail.com>
2024-11-29 15:34:00 +0100

    fixed a gap in geometry

---
+++ src/Shared/Functions/Display.cs
@@ -0,0 +1,10 @@
+﻿namespace Functions
+{
+    public void Komunikat1()
+    {
+        Console.Writeline("1 - Wyświetl");
+        Console.Writeline("2 - Dodaj");
+        Console.Writeline("3 - Usuń");
+        Console.Writeline("4 - Edytuj");
+    }
+}
--- src/TransMlecz/Program.cs
+++ src/TransMlecz/Program.cs
@@ -1,6 +1,6 @@
 ﻿// GŁÓWNY PLIK .cs //
 //MENU GŁÓWNE
-int i = 0, j = 0;
+int string i = "0", j = "0" ;
 bool t = true;
 Console.Writeline("-------Witaj-------");
 Console.Writeline("Wybierz co chcesz zrobić");
@@ -10,15 +10,12 @@ Console.Writeline("2 - Pojazdy");
 Console.WriteLine("3 - Zlecenia");
 while (t==true;)
 {
-    cin >> i;
+    i = Console.ReadLine()
     switch (i)
     {
         case 1:
-            Console.Writeline("1 - Wyświetl");
-            Console.Writeline("2 - Dodaj");
-            Console.Writeline("3 - Usuń");
-            Console.Writeline("4 - Edytuj");
-            cin >> j;
+            Display.Komunikat1();
+            j = Console.ReadLine();
             //ODWOŁANIE DO ODPOWIEDNIEJ FUNKCJI//
             break;
         case 2:
@@ -26,7 +23,7 @@ while (t==true;)
             Console.Writeline("2 - Dodaj");
             Console.Writeline("3 - Usuń");
             Console.Writeline("4 - Edytuj");
-            cin >> j;
+            j = Console.ReadLine();
             //ODWOŁANIE DO ODPOWIEDNIEJ FUNKCJI//
             break;
         case 3:
@@ -34,7 +31,7 @@ while (t==true;)
             Console.Writeline("2 - Dodaj");
             Console.Writeline("3 - Usuń");
             Console.Writeline("4 - Edytuj");
-            cin >> j;
+            j = Console.ReadLine();
             //ODWOŁANIE DO ODPOWIEDNIEJ FUNKCJI//
             break;
         case 4: 
