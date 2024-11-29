0523708214c788001b588a4f451c59f54dfcaaf9
mosuchowski <mosuchowski04@gmail.com>
2024-11-29 15:17:25 +0100

    fixed file structure

---
+++ src/Shared/Entities/DriverEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Driver
+    {
+        public int Id;
+        public string Name;
+        public string Surname;
+        public bool CertificateA;
+        public bool CertificateB;
+
+    }
+}
